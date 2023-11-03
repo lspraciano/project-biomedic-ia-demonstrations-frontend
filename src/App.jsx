6a8a4b8ca32e792e51a02cfbe1580f170c4d8081
Lucas Praciano <luskcct@gmail.com>
2023-11-03 10:19:27 -0300

    feat: iniciando aplicação

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+    return (
+        <>
+            <div>
+                <h1>
+                    Oi
+                </h1>
+            </div>
+        </>
+    )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import './colorsPallete/colors-palette.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+)
